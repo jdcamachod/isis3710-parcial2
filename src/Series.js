2dda3ca915816b1df8f4ff83d9f73d6c3f8ed58d
Juan Diego Camacho <jd.camachod@uniandes.edu.co>
2021-05-25 13:06:25 -0500

    first commit

---
+++ src/Series.js
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+
+function Series() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [series, setseries] = useState([]);
+
+
+}
